96346c9afa3f282306250a82c7bfead2a5d224bc
moaz <moaz43700@gmail.com>
2025-04-07 01:14:29 +0200

    03-Department Module(Entities&Config)&DbContext

---
+++ IKEA.DAL/Data/Configurations/DepartmentConfig.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace IKEA.DAL.Data.Configurations
+{
+    class DepartmentConfig : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id)
+                   .UseIdentityColumn(10, 10);
+
+            builder.Property(D => D.Name)
+                   .HasColumnType("varchar(20)");
+
+            builder.Property(D => D.Code)
+                   .HasColumnType("varchar(20)");
+
+            builder.Property(D => D.CreatedOn)
+                   .HasDefaultValueSql("GETDATE()");
+
+            builder.Property(D => D.LastModifiedOn)
+                   .HasComputedColumnSql("GETDATE()");
+        }
+    }
+}
---
+++ IKEA.DAL/Data/Contexts/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace IKEA.DAL.Data.Contexts
+{
+    class ApplicationDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("connection string");
+        }
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
