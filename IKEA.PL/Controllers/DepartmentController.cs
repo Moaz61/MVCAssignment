e0afb4e139c1d1bea2ad3e6c47e08fb9f48465bf
moaz <moaz43700@gmail.com>
2025-04-11 14:29:06 +0200

    04-Department Controller Create

--- IKEA.PL/Controllers/DepartmentController.cs
+++ IKEA.PL/Controllers/DepartmentController.cs
@@ -1,15 +1,56 @@
-﻿using IKEA.BLL.Services;
+﻿using IKEA.BLL.DataTransferObjects;
+using IKEA.BLL.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IKEA.PL.Controllers
 {
-    public class DepartmentController(IDepartmentService _departmentService) : Controller
+    public class DepartmentController(IDepartmentService _departmentService ,
+        ILogger<DepartmentController> _logger ,
+        IWebHostEnvironment _environment): Controller
     {
-        //BaseURL/Departments/Index
+        //BaseURL/Department/Index
         public IActionResult Index()
         {
             var departments = _departmentService.GetAllDepartments();
             return View(departments);
         }
+
+        #region Create Department
+        [HttpGet]
+        public IActionResult Create() => View();
+        
+        [HttpPost]
+        public IActionResult Create(CreatedDepartmentDto departmentDto)
+        {
+            if(ModelState.IsValid) //Server Side Validation
+            { 
+                try
+                {
+                    int Result = _departmentService.AddDepartment(departmentDto);
+                    if (Result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Department Can't Be Created");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    if(_environment.IsDevelopment())
+                    {
+                        // 1. Development => Log Error In Console And Return Same View With Error Message
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                    }
+                    else
+                    {
+                        // 2. Deployment => Log Error In File | Table In Database And Return Error View
+                        _logger.LogError(ex.Message);
+                    }
+                }
+            }
+            return View(departmentDto);  
+        }
+
+        #endregion
     }
 }
