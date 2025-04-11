76218c782708d382f65c80ff59f46d8f58ca4c74
moaz <moaz43700@gmail.com>
2025-04-11 05:54:21 +0200

    03-Department Controller Index

---
+++ IKEA.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,15 @@
+﻿using IKEA.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKEA.PL.Controllers
+{
+    public class DepartmentController(IDepartmentService _departmentService) : Controller
+    {
+        //BaseURL/Departments/Index
+        public IActionResult Index()
+        {
+            var departments = _departmentService.GetAllDepartments();
+            return View(departments);
+        }
+    }
+}
