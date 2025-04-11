62034ad92e7119ed178174f4c06933ac609d7893
moaz <moaz43700@gmail.com>
2025-04-11 18:08:26 +0200

    Details Change

--- IKEA.PL/Controllers/DepartmentController.cs
+++ IKEA.PL/Controllers/DepartmentController.cs
@@ -52,5 +52,16 @@ namespace IKEA.PL.Controllers
         }
 
         #endregion
+
+        #region Details Of Department
+        [HttpGet]
+        public IActionResult Details(int? id)
+        { 
+            if(!id.HasValue) return BadRequest(); //400
+            var department = _departmentService.GetDepartmentById(id.Value);
+            if (department is null) return NotFound(); //404
+            return View(department);
+        }
+        #endregion
     }
 }
