49bae7ab20f53b456fb2b600d1eb0534ffb6ffdd
moaz <moaz43700@gmail.com>
2025-04-20 22:40:21 +0200

    03-Employee Create Action

--- IKEA.DAL/Models/EmployeeModel/EmployeeType.cs
+++ IKEA.DAL/Models/EmployeeModel/EmployeeType.cs
@@ -9,6 +9,7 @@ namespace IKEA.DAL.Models.EmployeeModel
     public enum EmployeeType
     {
         FullTime = 1,
-        PartTime = 2
+        PartTime = 2,
+        Remote = 3
     }
 }
--- IKEA.PL/Controllers/EmployeesController.cs
+++ IKEA.PL/Controllers/EmployeesController.cs
@@ -1,14 +1,45 @@
-﻿using IKEA.BLL.Services.Interfaces;
+﻿using IKEA.BLL.DataTransferObjects.DepartmentDtos;
+using IKEA.BLL.DataTransferObjects.EmployeeDtos;
+using IKEA.BLL.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IKEA.PL.Controllers
 {
-    public class EmployeesController(IEmployeeService _employeeService) : Controller
+    public class EmployeesController(IEmployeeService _employeeService , IWebHostEnvironment environment , ILogger<EmployeesController> logger) : Controller
     {
         public IActionResult Index()
         {
             var Employees = _employeeService.GetAllEmployees();
             return View(Employees);
         }
+
+        #region Create Employee
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(CreatedEmployeeDto employeeDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int Result = _employeeService.CreateEmployee(employeeDto);
+                    if (Result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                        ModelState.AddModelError(string.Empty, "Can't Create Employee");
+                }
+                catch (Exception ex)
+                {
+                    if (environment.IsDevelopment()) 
+                        ModelState.AddModelError(string.Empty , ex.Message);
+                    else
+                        logger.LogError(ex.Message);
+                }
+            }
+            return View(employeeDto);
+        }
+        #endregion
     }
 }
